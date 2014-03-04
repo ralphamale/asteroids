d04b2244386acccf7e17cf06517a175dfa04fac4
App Academy Student <student@appacademy.io>
2014-03-04 12:15:01 -0500

    Major refactoring. Still working on niceties.

--- keybinds.js
+++
@@ -1,9 +0,0 @@
-var xbullets = Asteroids.Game.bullets;
-
-  key('a', function(){
-    //key.setScope(Asteroids);
-    //this.Game.bullets.push(Asteroids.Ship.fireBullet())
-
-    alert(xbullets);
-
-  });
